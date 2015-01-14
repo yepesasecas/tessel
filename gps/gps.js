2420ee042aed6339f34fc86236358f299171e958
Andres Yepes <yepes07@gmail.com>
2015-01-14 12:02:28 -0500

    create gps tutorial

---
+++ gps/gps.js
@@ -0,0 +1,33 @@
+var tessel = require("tessel");
+var gpslib = require("gps-a2235h");
+var gps    = gpslib.use(tessel.port["C"]);
+
+gpslib.debug = 1;
+
+gps.on("ready", function(){
+  console.log('GPS module powered and ready. Waiting for satellites...');
+
+  // Emit coordinates when we get a coordinate fix
+  gps.on('coordinates', function (coords) {
+    console.log('Lat:', coords.lat, '\tLon:', coords.lon, '\tTimestamp:', coords.timestamp);
+  });
+
+  // Emit altitude when we get an altitude fix
+  gps.on('altitude', function (alt) {
+    console.log('Got an altitude of', alt.alt, 'meters (timestamp: ' + alt.timestamp + ')');
+  });
+
+  // Emitted when we have information about a fix on satellites
+  gps.on('fix', function (data) {
+    console.log(data.numSat, 'fixed.');
+  });
+
+  gps.on('dropped', function(){
+    // we dropped the gps signal
+    console.log("gps signal dropped");
+  });
+});
+
+gps.on('error', function(err){
+  console.log("got this error", err);
+});
